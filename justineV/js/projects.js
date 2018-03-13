42d8240b0c4e07ac65196cfcc7c4c0b282c64dc7
Axel Vencatareddy <axel@koosmikcorp.com>
2018-03-14 06:37:53 +0800

    Add beggining of project page

--- justineV/js/projects.js
+++ justineV/js/projects.js
@@ -1,11 +1,22 @@
 $(function() {
-  $(".img-w").each(function() {
+  $('.img-w:not(.big-project):not(.little-project)').each(function() {
     $(this).wrap("<div class='img-c'></div>");
-    let imgSrc = $(this).find("img").attr("src");
-     $(this).css('background-image', 'url(' + imgSrc + ')');
-  })
-
+    let imgSrc = $(this).find('img').attr('src');
+    $(this).css('background-image', 'url(' + imgSrc + ')');
   });
+
+  $('.img-w.big-project').each(function() {
+    $(this).wrap("<div class='img-c img-c-big'></div>");
+    let imgSrc = $(this).find('img').attr('src');
+    $(this).css('background-image', 'url(' + imgSrc + ')');
+  });
+
+  $('.img-w.little-project').each(function() {
+    $(this).wrap("<div class='img-c img-c-little'></div>");
+    let imgSrc = $(this).find('img').attr('src');
+    $(this).css('background-image', 'url(' + imgSrc + ')');
+  });
+});
 
 window.onload = function()
 {
