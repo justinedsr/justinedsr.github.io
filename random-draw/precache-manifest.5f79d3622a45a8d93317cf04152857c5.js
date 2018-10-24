6bd61d1414e8ef6200cf20ef64e4b5708ff6fdfd
Axel Vencatareddy <avencatareddy@me.com>
2018-10-24 09:23:48 +0200

    Add random-draw tools

---
+++ random-draw/precache-manifest.5f79d3622a45a8d93317cf04152857c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e986504270e0dda1adc",
+    "url": "/random-draw/static/js/runtime~main.6e986504.js"
+  },
+  {
+    "revision": "a126f90ed892fef5e97b",
+    "url": "/random-draw/static/js/main.a126f90e.chunk.js"
+  },
+  {
+    "revision": "94c8180847106eb35c9d",
+    "url": "/random-draw/static/js/1.94c81808.chunk.js"
+  },
+  {
+    "revision": "a126f90ed892fef5e97b",
+    "url": "/random-draw/static/css/main.de76adb4.chunk.css"
+  },
+  {
+    "revision": "e551bbec579bcbc6860678404a8b2ba8",
+    "url": "/random-draw/index.html"
+  }
+];
